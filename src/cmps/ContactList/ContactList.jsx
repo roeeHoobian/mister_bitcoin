adbbb154e9426b6a9a03f54f4c888bf71450d1ce
roee-hoobian <hoobizone@gmail.com>
2020-12-17 17:44:45 +0200

    sdf

---
+++ src/cmps/ContactList/ContactList.jsx
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react'
+
+import './ContactList.css'
+
+class ContactList extends Component {
+
+    render() {
+        <h1>contact list cmp</h1>
+    }
+}
+
+export default ContactList
